3b86106b926a9516b94396bc31f3e7755ebbe6a5
01gap <paola.pogli@gmail.com>
2020-07-02 21:24:13 +0300

    Не работает валидация хэштегов

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@
   var MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
   var NAMES = ['Dub Dubom', 'Василий Петрович', 'Генрих IV', 'Анюта', 'Анонимус', 'Финдус'];
 
-  var usersImagesTitle = document.querySelector('.pictures__title');
-  usersImagesTitle.classList.remove('visually-hidden');
+  var createRandom = function (min, max) {
+    var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumber;
+  };
 
   var createImgMessage = function () {
     var msg = '';
-    for (var k = 0; k < Math.floor(Math.random() * 2); k++) {
+    for (var i = 0; i < createRandom(0, 1); i++) {
       msg += MESSAGES[Math.floor(Math.random() * MESSAGES.length)];
     }
     return msg;
@@ -18,8 +20,8 @@
 
   var createImgComment = function () {
     var comments = [];
-    for (var j = 0; j < Math.floor(Math.random() * 10); j++) {
-      comments[j] = {
+    for (var i = 0; i < createRandom(0, 9); i++) {
+      comments[i] = {
         avatar: 'img/avatar-' + Math.floor(Math.random() * 6 + 1) + '.svg',
         name: NAMES[Math.floor(Math.random() * NAMES.length)],
         message: createImgMessage(),
@@ -27,16 +29,18 @@
     }
     return comments;
   };
+  var createImageUnit = function (imgIndex) {
+    var imageUnit = {
+      url: 'photos/' + imgIndex + '.jpg',
+      description: '',
+      likes: createRandom(15, 200),
+      comments: createImgComment(),
+    };
+    return imageUnit;
+  };
+
   window.data = {
-    createImageUnit: function (imgIndex) {
-      var imageUnit = {
-        url: 'photos/' + imgIndex + '.jpg',
-        description: '',
-        likes: Math.floor(Math.random() * (200 - 15 + 1) + 15),
-        comments: createImgComment(),
-      };
-      return imageUnit;
-    },
+    createImageUnit: createImageUnit
   };
 })();
 
@@ -67,21 +71,20 @@
 (function () {
   var bodyBizarre = document.querySelector('body');
   var imgEditing = document.querySelector('.img-upload__overlay');
-  // var uploadLabel = document.querySelector('.img-upload__label');
   var imgEditingCancel = imgEditing.querySelector('.img-upload__cancel');
   var imgUploadInput = document.querySelector('.img-upload__input');
   var uploadedImage = document.querySelector('.img-upload__preview img');
+  // var hashtagInput = document.querySelector('.text__hashtags');
+  var uploadComment = document.querySelector('.text__description');
 
   var onImgEditingEscPress = function (evt) {
-    if (evt.key === 'Escape') {
+    if (evt.key === 'Escape' && document.activeElement !== uploadComment && document.activeElement !== hashtagInput) {
       evt.preventDefault();
       closeImgEditing();
     }
   };
 
   imgUploadInput.addEventListener('change', function () {
-    // так я пыталась добавить загрузку стороннего изображения. Адрес изображения в DOM появляется, но консоль выдает ошибку: net::ERR_FILE_NOT_FOUND
-    // uploadedImage.src = imgUploadInput.value;
     imgEditing.classList.remove('hidden');
     bodyBizarre.classList.add('modal-open');
     document.addEventListener('keydown', onImgEditingEscPress);
@@ -107,7 +110,7 @@
   var filterList = document.querySelector('.effects__list');
 
 
-  var applyFilter = function (evt) {
+  var onChangeApplyFilter = function (evt) {
     if (evt.target.value === 'none') {
       uploadedImage.setAttribute('class', '');
       effectLevelSlider.classList.add('hidden');
@@ -117,26 +120,27 @@
     }
   };
 
-  filterList.addEventListener('change', applyFilter);
+  filterList.addEventListener('change', onChangeApplyFilter);
 
 
+  var form = document.querySelector('.img-upload__form');
+  var buttonSubmit = document.querySelector('.img-upload__submit');
   var hashtagInput = document.querySelector('.text__hashtags');
 
-  hashtagInput.addEventListener('input', function () {
-    var isValid = true;
-    if (hashtagInput.value !== '' && isValid) {
+  buttonSubmit.addEventListener('click', function (evt) {
+    if (hashtagInput.value !== '') {
       var hashtags = hashtagInput.value.trim().split(' ');
-      var hashtagRegex = /^#[A-ZА-Я0-9ё]{2,20}$/i;
+      var hashtagRegex = /^#[A-ZА-Я0-9ё]{1,19}$/i;
       for (var i = 0; i < hashtags.length; i++) {
-        if (hashtagRegex.test(hashtags[i]) === false) {
-          isValid = false;
+        if (!hashtagRegex.test(hashtags[i])) {
+          hashtagInput.setCustomValidity('ошибка');
+          evt.preventDefault();
+        } else {
+          hashtagInput.setCustomValidity('');
         }
       }
     }
-    if (isValid) {
-      hashtagInput.setCustomValidity('');
-    } else {
-      hashtagInput.setCustomValidity('В хэштеге допущена ошибка');
-    }
   });
 })();
+
+
