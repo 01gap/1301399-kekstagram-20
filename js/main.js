25932b2bf76bfe3a5a1556c419eb106670288127
01gap <paola.pogli@gmail.com>
2020-07-03 14:51:50 +0300

    Поравлены хэштеги: #ddd = #DDD

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@
     for (var i = 0; i < arr.length; i++) {
       for (var j = 0; j < arr.length; j++) {
         if (i !== j) {
-          if (arr[i] === arr[j]) {
+          if (arr[i].toLowerCase() === arr[j].toLowerCase()) {
             return false;
           }
         }
