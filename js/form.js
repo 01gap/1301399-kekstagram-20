2cd3821f01670ef4eff522e53e0882344d71c40e
01gap <paola.pogli@gmail.com>
2020-07-18 01:42:46 +0300

    правки

--- js/form.js
+++ js/form.js
@@ -3,6 +3,7 @@
 // form.js модуль работы с формой
 
 (function () {
+  var MAX_EFFECT_VALUE = 100;
   var imgEditing = document.querySelector('.img-upload__overlay');
   var imgEditingCancel = imgEditing.querySelector('.img-upload__cancel');
   var imgUploadInput = document.querySelector('.img-upload__input');
@@ -13,6 +14,54 @@
   var effectLevelSlider = document.querySelector('.img-upload__effect-level');
   var previewOriginal = document.querySelector('#effect-none');
   var filterList = document.querySelector('.effects__list');
+
+  var effectLine = document.querySelector('.effect-level__line');
+  var effectPin = effectLine.querySelector('.effect-level__pin');
+  var effectDepth = effectLine.querySelector('.effect-level__depth');
+  var effectValue = document.querySelector('.effect-level__value');
+  var currentEffect;
+
+  var GRAYSCALE = {
+    name: 'grayscale',
+    min: 0,
+    max: 1,
+    unit: ''
+  };
+  var SEPIA = {
+    name: 'sepia',
+    min: 0,
+    max: 1,
+    unit: ''
+  };
+
+  var INVERT = {
+    name: 'invert',
+    min: 0,
+    max: 100,
+    unit: '%'
+  };
+
+  var BLUR = {
+    name: 'blur',
+    min: 0,
+    max: 3,
+    unit: 'px'
+  };
+
+  var BRIGHTNESS = {
+    name: 'brightness',
+    min: 1,
+    max: 2,
+    unit: ''
+  };
+
+  var FILTERS = {
+    chrome: GRAYSCALE,
+    sepia: SEPIA,
+    marvin: INVERT,
+    phobos: BLUR,
+    heat: BRIGHTNESS
+  };
 
   var closeImgEditing = function () {
     imgEditing.classList.add('hidden');
@@ -40,15 +89,20 @@
     closeImgEditing();
   });
 
-  var appliedFilter;
   var onChangeApplyFilter = function (evt) {
-    appliedFilter = evt.target.value;
-    if (appliedFilter === 'none') {
+    currentEffect = evt.target.value;
+    if (currentEffect === 'none') {
       uploadedImage.setAttribute('class', '');
+      removeEffectValue();
       effectLevelSlider.classList.add('hidden');
     } else {
-      uploadedImage.setAttribute('class', 'effects__preview--' + appliedFilter);
+      uploadedImage.setAttribute('class', 'effects__preview--' + currentEffect);
       effectLevelSlider.classList.remove('hidden');
+      effectValue.value = MAX_EFFECT_VALUE;
+      effectPin.style.left = MAX_EFFECT_VALUE + '%';
+      effectDepth.style.width = MAX_EFFECT_VALUE + '%';
+      addEffectValue(currentEffect);
+      effectPin.addEventListener('mousedown', onMouseDown);
     }
   };
 
@@ -88,71 +142,28 @@
     }
   });
 
-  var effectLine = document.querySelector('.effect-level__line');
-  var effectPin = effectLine.querySelector('.effect-level__pin');
-  var effectDepth = effectLine.querySelector('.effect-level__depth');
-  var effectValue = document.querySelector('.effect-level__value');
-  var GRAYSCALE = {
-    min: 0,
-    max: 1,
-    unit: ''
-  };
-  var SEPIA = {
-    min: 0,
-    max: 1,
-    unit: ''
+  var addEffectValue = function (effect) {
+    var ratio = effectValue.value / 100;
+    uploadedImage.style.filter = FILTERS[effect].name + '(' + (FILTERS[effect].min + ratio * (FILTERS[effect].max - FILTERS[effect].min)) + FILTERS[effect].unit + ')';
   };
 
-  var INVERT = {
-    min: 0,
-    max: 100,
-    unit: '%'
+  var removeEffectValue = function () {
+    uploadedImage.style.filter = '';
   };
 
-  var BLUR = {
-    min: 0,
-    max: 3,
-    unit: 'px'
-  };
-
-  var BRIGHTNESS = {
-    min: 1,
-    max: 2,
-    unit: ''
-  };
-
-  var FILTERS = {
-    chrome: GRAYSCALE,
-    sepia: SEPIA,
-    marvin: INVERT,
-    phobos: BLUR,
-    heat: BRIGHTNESS
-  };
-
-  var addFilterValue = function (effect, ratio) {
-    uploadedImage.style.filter = effect + '(' + FILTERS.effect.min + ratio * (FILTERS.effect.max - FILTERS.effect.min) + FILTERS.effect.unit + ')';
-  };
-
-
-  effectPin.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-    var pinStart = effectPin.getBoundingClientRect();
-    var line = effectLine.getBoundingClientRect();
-    var pinStartRelX = pinStart.x - line.x;
+  var onMouseDown = function (downEvt) {
+    downEvt.preventDefault();
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
-      var shiftX = moveEvt.clientX - pinStart.x;
-      var relCoordX = pinStartRelX + shiftX;
-      var restrictedCoordX = Math.max(0, Math.min(line.width, relCoordX));
+      var line = effectLine.getBoundingClientRect();
+      var pinCoordX = moveEvt.clientX - line.x;
+      var restrictedCoordX = Math.max(0, Math.min(line.width, pinCoordX));
       effectPin.style.left = restrictedCoordX + 'px';
       effectDepth.style.width = restrictedCoordX + 'px';
       var depthPercentage = Math.round(restrictedCoordX * 100 / line.width);
-      var depthRatio = depthPercentage / 100;
       effectValue.value = depthPercentage;
-      // главный вопрос: как получить (куда сохранить) имя примененного
-      // эффекта из onChangeApplyFilter, чтобы использовать его здесь ?
-      // addFilterValue(appliedFilter, depthRatio);
+      addEffectValue(currentEffect);
     };
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
@@ -161,6 +172,6 @@
     };
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
-  });
+  };
 
 })();
